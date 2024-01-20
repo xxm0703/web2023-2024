59737ad441ea257fee6d46b4a6693fe3a8ee251a
angeldp1 <a4atawe@gmail.com>
2024-01-20 18:23:01 +0200

    Add not null constraints and cascade delete

--- src/backend/createTables.php
+++ src/backend/createTables.php
@@ -10,58 +10,57 @@ function createTable($pdo, $tableName, $sql) {
 }
 
 function createTables($pdo) {
-
-$tables = array(
-    "projects" => "
-      CREATE TABLE projects (
-        id INTEGER PRIMARY KEY,
-        name VARCHAR(255),
-        created_at TIMESTAMP
-      );
-    ",
-    "requirements" => "
-      CREATE TABLE requirements (
-        id INTEGER PRIMARY KEY,
-        name VARCHAR(255),
-        description VARCHAR(255),
-        project_id INTEGER,
-        created_at TIMESTAMP,
-        FOREIGN KEY (project_id) REFERENCES projects(id)
-      );
-    ",
-    "functional_requirements" => "
-      CREATE TABLE functional_requirements (
-        id INTEGER PRIMARY KEY,
-        requirement_id INTEGER,
-        FOREIGN KEY (requirement_id) REFERENCES requirements(id)
-      );
-    ",
-    "nonfunctional_requirements" => "
-      CREATE TABLE nonfunctional_requirements (
-        id INTEGER PRIMARY KEY,
-        unit VARCHAR(255),
-        value VARCHAR(255),
-        requirement_id INTEGER,
-        FOREIGN KEY (requirement_id) REFERENCES requirements(id)
-      );
-    ",
-    "hash_tags" => "
-      CREATE TABLE hash_tags (
-        id INTEGER PRIMARY KEY,
-        name VARCHAR(255) UNIQUE,
-        created_at TIMESTAMP
-      );
-    ",
-    "hash_tags_requirements" => "
-      CREATE TABLE hash_tags_requirements (
-        id INTEGER PRIMARY KEY,
-        hash_tag_id INTEGER,
-        requirement_id INTEGER,
-        FOREIGN KEY (hash_tag_id) REFERENCES hash_tags(id),
-        FOREIGN KEY (requirement_id) REFERENCES requirements(id)
-      );
-    "
-  );
+	$tables = array(
+		"projects" => "
+		CREATE TABLE projects (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			name VARCHAR(255) NOT NULL,
+			created_at TIMESTAMP
+		);
+		",
+		"requirements" => "
+		CREATE TABLE requirements (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			name VARCHAR(255) NOT NULL,
+			description VARCHAR(255),
+			project_id INTEGER NOT NULL,
+			created_at TIMESTAMP,
+			FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+		);
+		",
+		"functional_requirements" => "
+		CREATE TABLE functional_requirements (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			requirement_id INTEGER NOT NULL,
+			FOREIGN KEY (requirement_id) REFERENCES requirements(id) ON DELETE CASCADE
+		);
+		",
+		"nonfunctional_requirements" => "
+		CREATE TABLE nonfunctional_requirements (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			unit VARCHAR(255) NOT NULL,
+			value VARCHAR(255) NOT NULL,
+			requirement_id INTEGER NOT NULL,
+			FOREIGN KEY (requirement_id) REFERENCES requirements(id) ON DELETE CASCADE
+		);
+		",
+		"hash_tags" => "
+		CREATE TABLE hash_tags (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			name VARCHAR(255) UNIQUE NOT NULL,
+			created_at TIMESTAMP
+		);
+		",
+		"hash_tags_requirements" => "
+		CREATE TABLE hash_tags_requirements (
+			id INTEGER PRIMARY KEY AUTO_INCREMENT,
+			hash_tag_id INTEGER NOT NULL,
+			requirement_id INTEGER NOT NULL,
+			FOREIGN KEY (hash_tag_id) REFERENCES hash_tags(id) ON DELETE CASCADE,
+			FOREIGN KEY (requirement_id) REFERENCES requirements(id) ON DELETE CASCADE
+		);
+		"
+	);
 
     foreach ($tables as $tableName => $sql) {
         createTable($pdo, $tableName, $sql);
